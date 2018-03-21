4cb184b9ebf8c5f16a0641361bfc8f973c17a445
Alex Haase <adhaase@umich.edu>
2018-03-21 03:17:17 -0400

    fixed the problem where the program couldn't handle decimals properly

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 // 360 degrees in X cm = Maximum value CM Per 360 / OW Sensitivity / DPI
 
+document.getElementById('output_id').style.visibility = 'hidden';
+
 function cmResult() {
+    //document.getElementById("output_id").addEventListener("click", cmResult);
+    document.getElementById('output_id').style.visibility = 'visible';
+    
     var maximumValPer360 = 138545.455;
-    var user_mDPI = parseInt(document.getElementById("mDPI_id").value);
-    var user_ingameSens = parseInt(document.getElementById("ingameSens_id").value);
+    var user_mDPI = parseFloat(document.getElementById("mDPI_id").value);
+    var user_ingameSens = parseFloat(document.getElementById("ingameSens_id").value);
     var cm360Result = maximumValPer360 / user_ingameSens / user_mDPI;
 
     document.getElementById("cm360Result_id").value = cm360Result.toFixed(2);
+    
+    // double checking values
+    console.log(user_mDPI);
+    console.log(user_ingameSens);
+    console.log(maximumValPer360)
 }
